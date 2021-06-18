af44d374aa8ba470961ec3379805ee28618a4da5
hieu-lee <hiuld1009@gmail.com>
2021-06-18 12:21:37 +0200

    quite done with send letter page

---
+++ FutureMe/Models/Letter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureMe.Models
+{
+    public class Letter
+    {
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public DateTime? DeliverDate { get; set; }
+        [Required]
+        public PrivacyMode Privacy { get; set; } = 0;
+    }
+}
---
+++ FutureMe/Models/PrivacyMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureMe.Models
+{
+    public enum PrivacyMode
+    {
+        Private = 0,
+        Anonymous = 1,
+        Public = 2
+    }
+}
