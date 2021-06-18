ea1bb1c58cd0a2f16bc83728ea04af5b888c352c
hieu-lee <hiuld1009@gmail.com>
2021-06-18 10:16:00 +0200

    add like functionality

---
+++ FutureMe/Models/Feedback.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureMe.Models
+{
+    public class Feedback
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime PostTime { get; set; } = DateTime.Now;
+        public int Likes { get; set; } = 0;
+    }
+}
