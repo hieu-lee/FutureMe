c1a0132023c0d696ed6ea1c178af1483d016fd65
hieu-lee <hiuld1009@gmail.com>
2021-06-22 09:14:47 +0200

    .

---
+++ FutureMe/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureMe.Models
+{
+    public class Account
+    {
+        [Required(ErrorMessage = "Bạn cần nhập email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Bạn cần nhập mật khẩu")]
+        [StringLength(15, ErrorMessage = "Your password must have length between 6 and 15", MinimumLength = 6)]
+        public string Password { get; set; }
+        public string Username { get; set; }
+    }
+}
