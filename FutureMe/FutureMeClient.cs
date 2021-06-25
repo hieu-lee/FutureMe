3e757eafe4c92bd2700b864577474b58c690ac28
hieu-lee <hiuld1009@gmail.com>
2021-06-25 23:11:21 +0200

    add login functionality

---
+++ FutureMe/FutureMeClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FutureMe
+{
+    public class FutureMeClient
+    {
+        public HttpClient UserClient { get; init; } = new()
+        {
+            BaseAddress = new Uri("https://futuremeuserapi.azurewebsites.net")
+        };
+
+        public HttpClient LetterAndFeedbackClient { get; init; } = new()
+        {
+            BaseAddress = new Uri("https://localhost:44376")
+        };
+
+        public FutureMeClient()
+        {
+            UserClient.DefaultRequestHeaders.Add("ApiKey", "Jeg5dSHhmNd6GueSDbe754PPDUFn9SUbwhz4Zwjgplhcute");
+        }
+    }
+}
---
+++ FutureMe/SessionData.cs
@@ -0,0 +1,21 @@
+﻿using FutureMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureMe
+{
+    public class SessionData
+    {
+        public Account MyAccount { get; set; }
+        public string Key { get; set; }
+        public bool Logged => (MyAccount is not null);
+
+        public void ClearData()
+        {
+            MyAccount = null;
+            Key = string.Empty;
+        }
+    }
+}
