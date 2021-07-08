52a3331c2d59e746925f652d12fa8458393a8555
hieu-lee <hiuld1009@gmail.com>
2021-07-08 23:49:35 +0200

    re deploy

--- FutureMe/SessionData.cs
+++ FutureMe/SessionData.cs
@@ -11,7 +11,7 @@ namespace FutureMe
 {
     public class SessionData
     {
-        public static string SessionKey = "FutureMeUserSessionKey";
+        public static string SessionKey = "FutureMeClientUserSessionKey";
         public Account MyAccount { get; set; }
         public string Key { get; set; }
         public bool Logged => (MyAccount is not null);
